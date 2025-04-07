7bcf3ce12ab84fe6d22158c8c8f110c15a27643e
mburnayev <misha@burnayev.com>
2025-04-07 01:16:36 -0400

    dud fix

--- hosting/src/Dud.jsx
+++ hosting/src/Dud.jsx
@@ -1,6 +1,6 @@
 function Dud() {
     return (
-        <img src="Dud.webp" width="100" height="120" style={{ float: "center", transform: "translateY(330px)" }} />
+        <img src="Dud.webp" width="100" height="160" style={{ float: "center", transform: "translateY(330px)" }} />
     );
 }
 
